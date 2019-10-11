a5b70e2b65b91bfc8af1776f2ca89da0bb9bbdbf
nky <1947647519@qq.com>
2019-10-11 20:27:19 +0800

    申请状态页面和报销状态页面

--- src/main/java/com/aca/springboot/component/LoginHandlerInterceptor.java
+++ src/main/java/com/aca/springboot/component/LoginHandlerInterceptor.java
@@ -13,7 +13,7 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
     //目标方法执行之前
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object user = request.getSession().getAttribute("loginUser");
+        /*Object user = request.getSession().getAttribute("loginUser");
         if(user == null){
             //未登陆，返回登陆页面
             request.setAttribute("msg","没有权限请先登陆");
@@ -22,8 +22,8 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
         }else{
             //已登陆，放行请求
             return true;
-        }
-
+        }*/
+        return true;
     }
 
     @Override
