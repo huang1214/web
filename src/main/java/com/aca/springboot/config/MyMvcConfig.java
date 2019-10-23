ae2061930a9cf80205385c6032e2e300fa071f72
nky <1947647519@qq.com>
2019-10-23 21:25:54 +0800

    报销页面 审核报销页面

--- src/main/java/com/aca/springboot/config/MyMvcConfig.java
+++ src/main/java/com/aca/springboot/config/MyMvcConfig.java
@@ -65,8 +65,8 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
                  */
                 registry.addViewController("admin/com_search").setViewName("/admin/com_search");
                 registry.addViewController("admin/audit").setViewName("/admin/audit");
+                registry.addViewController("admin/bill_audit").setViewName("/admin/bill_audit");
             }
-
             //注册拦截器
             @Override
             public void addInterceptors(InterceptorRegistry registry) {
