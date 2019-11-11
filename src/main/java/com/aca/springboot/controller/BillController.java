a38b934fd0d52abff22978105b8d5cdf801d34c1
NeglectOverlook <abraham.zhou23@gmail.com>
2019-11-11 11:07:21 +0800

    周全-根据黄老师要求第3次修改，优化一些细节

--- src/main/java/com/aca/springboot/controller/BillController.java
+++ src/main/java/com/aca/springboot/controller/BillController.java
@@ -7,7 +7,6 @@ import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
